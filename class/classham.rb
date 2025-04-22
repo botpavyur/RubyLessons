bb7b968f10868210bf5bbd9541713bd84119ed46
botpavyur <botpavyur@gmail.com>
2025-04-22 10:24:08 +0200

    change chameleon's color using a class method

---
+++ class/classham.rb
@@ -0,0 +1,11 @@
+class Chameleom
+  def recolor(color)
+   puts "Теперь я #{(color)}"
+  end
+end
+
+chameleom = Chameleom.new
+
+chameleom.recolor('красный')
+chameleom.recolor('белый')
+chameleom.recolor('синий')
