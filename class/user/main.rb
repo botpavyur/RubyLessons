cfd111f9bf9c2202023ca11f4782ae5bc7d88a31
botpavyur <botpavyur@gmail.com>
2025-04-24 13:28:28 +0200

    main file

---
+++ class/user/main.rb
@@ -0,0 +1,12 @@
+require "./main.rb"
+
+user1 = User.new('Pavel', 65)
+user2 = User.new('Bogdan', 61)
+user3 = User.new('Ilya', 18)
+
+puts "we have a 3 person"
+
+user1.username
+user2.username
+user3.username
+
