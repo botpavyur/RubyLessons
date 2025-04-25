754bfeec406ac834de74b6dfb814c2928a566906
botpavyur <botpavyur@gmail.com>
2025-04-25 13:38:34 +0200

    main file for movies

---
+++ class/movies/main.rb
@@ -0,0 +1,14 @@
+class Movies
+  def initialize(producer, name_movies)
+   @producer = producer
+   @name_movies = name_movies
+  end
+  
+  def name_movies
+    @name_movies
+  end
+
+  def producer
+    @producer
+  end
+end
