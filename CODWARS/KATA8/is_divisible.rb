e6c82196983fa0904415b1b48c1e9f73456233e4
botpavyur <botpavyur@gmail.com>
2025-07-17 10:00:31 +0200

    check if a number is divisible

---
+++ CODWARS/KATA8/is_divisible.rb
@@ -0,0 +1,10 @@
+# Create a function that checks if a number n is divisible by two numbers x AND y.
+# All inputs are positive, non-zero numbers.
+
+def is_divisible(n,x,y)
+  if n % x == 0 && n % y == 0
+    true
+    else
+    false
+    end 
+  end
