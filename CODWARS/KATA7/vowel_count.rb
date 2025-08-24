cd469d65343d83365a6563ad9bc3773bc2804189
botpavyur <botpavyur@gmail.com>
2025-08-24 14:52:14 +0200

    traning kata 7

---
+++ CODWARS/KATA7/vowel_count.rb
@@ -0,0 +1,6 @@
+#Return the number (count) of vowels in the given string.
+#We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+def get_count(input_str)
+  p input_str.count('aeiou')
+end
